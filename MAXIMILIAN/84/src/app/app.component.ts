ecb2a6ef9ab5a05166f4c050fc01fe7edd0d046b
Aleksey <Elchin>
2021-12-21 07:26:27 +0900

    angular notebook

--- MAXIMILIAN/84/src/app/app.component.ts
+++ MAXIMILIAN/84/src/app/app.component.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
+  loadFeature: string = "recipe";
+
+  onNavigate(feature: string): void {
+    this.loadFeature = feature;
+  }
 }
