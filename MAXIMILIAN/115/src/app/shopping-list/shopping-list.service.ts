3a1816589933fe56c8d33d78051c844914d9312e
Aleksey <Elchin>
2021-12-29 08:27:28 +0900

    angular notebook

--- MAXIMILIAN/115/src/app/shopping-list/shopping-list.service.ts
+++ MAXIMILIAN/115/src/app/shopping-list/shopping-list.service.ts
@@ -1,1 +1,28 @@
-export class ShoppingListService {}
+import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
+
+export class ShoppingListService {
+  ingredientsChanged = new EventEmitter<Array<Ingredient>>();
+
+  private ingredients: Array<Ingredient> = [
+    new Ingredient("Apples", 5),
+    new Ingredient("Tomatoes", 10),
+  ];
+
+  getIngredients() {
+    return this.ingredients.slice();
+  }
+
+  addIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+    this.ingredientsChanged.emit(this.ingredients.slice());
+  }
+
+  addIngredients(ingredients: Array<Ingredient>) {
+    // for (let ingredient of ingredients) {
+    //   this.addIngredient(ingredient);
+    // }
+    this.ingredients.push(...ingredients);
+    this.ingredientsChanged.emit(this.ingredients.slice());
+  }
+}
